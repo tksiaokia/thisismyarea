63f8767b5b97d3df3b19a6e7617b9181dd06b86a
sean.tee <sean.tee@epochaltech.com>
2019-07-08 16:00:18 +0800

    first commit with simple stuff

---
+++ ThisIsMyArea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThisIsMyArea
+//
+//  Created by Sean Tee on 08/07/2019.
+//  Copyright © 2019 Sean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
